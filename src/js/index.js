034affcd34f5fc7da919bb53565ff65c791e4e01
Patryk Stefanowicz <patrykstefanowicz95@gmail.com>
2021-02-18 20:14:11 +0100

    javascript

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,40 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+
+const glassCounter = document.querySelector('.glass__counter--js');
+const buttonAdd = document.querySelector('.button-add--js');
+const buttonRemove = document.querySelector('.button-remove--js');
+
+const key = new Date().toISOString().slice(0, 10);
+
+let currentGlassCounter = 0; 
+
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue) {
+    currentGlassCounter = localStorageValue;
+} else {
+    localStorage.setItem(key , 0);
+}
+
+glassCounter.innerHTML = currentGlassCounter;
+
+console.log(key);
+
+
+buttonAdd.addEventListener('click', () => {
+    currentGlassCounter++;
+    glassCounter.innerHTML = currentGlassCounter;
+    localStorage.setItem(key , currentGlassCounter);
+});
+
+buttonRemove.addEventListener('click', () => {
+    if (currentGlassCounter > 0){
+        currentGlassCounter--;
+    }
+    glassCounter.innerHTML = currentGlassCounter;
+    localStorage.setItem(key , currentGlassCounter);
+})
+
+
